948e5b8be5df56ebfcf366b77e4987bd1f9d261b
loris <loris@localhost>
2024-04-22 23:02:14 +0200

    Demo to JSON import

--- APL/src/mainProgram/Main.java
+++ APL/src/mainProgram/Main.java
@@ -29,7 +29,8 @@ public class Main
 	//
 	public static void main(String[] args)
 	{
-		Start();
+		translation.selectLanguage("DE");
+		
 		
 		
 	}
--- APL/src/mainProgram/Translation.java
+++ APL/src/mainProgram/Translation.java
@@ -1,20 +1,38 @@
 package mainProgram;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;	
+import org.json.simple.parser.ParseException;
 
 
 public final class Translation 
 {
 	private static HashMap<String, String> translation = new HashMap<>();
 	
+	// ACHTUNG NOCH NICHT DYNAMISCH !!!
+	private static final String path = "/Users/loris/git/repository/APLII/APL/src/mainProgram/translation.json";
 	
 	
 	private void readFile(String lang)
 	{
+		try 
+		{
+			FileReader reader = new FileReader(path);
+			JSONParser parser = new JSONParser(); 
+			JSONObject object = (JSONObject)(parser.parse(reader));
+			
+            JSONObject o = (JSONObject)(object.get("PROG_NAME"));
+            System.out.println(o.get(lang));
+            
+            
+	    } 
+	 	catch (IOException | ParseException e) 
+	 	{
+            e.printStackTrace();
+        }
 		
 	}
 	
