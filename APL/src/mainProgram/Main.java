4c9930ed8e47ad37d63cdb2692fa2ea96d9c09af
loris <loris@localhost>
2024-04-23 10:36:18 +0200

    Minor changes MainMenu

--- APL/src/mainProgram/Main.java
+++ APL/src/mainProgram/Main.java
@@ -8,7 +8,7 @@ public class Main
 	public static Translation translation = new Translation();
 	
 	//
-	public static void Line()
+	public static void PrintLine()
 	{
 		for(int i = 0; i < 20; i++)
 		{
@@ -20,12 +20,15 @@ public class Main
 	
 	public static void PrintMainMenu()
 	{
+		PrintLine();
 		System.out.println(translation.getText("PROG_NAME"));
-		Line();
+		PrintLine();
 		
-		System.out.println("1: " + translation.getText("MAIN_MENU_CHANGE_LANGUAGE"));
-		System.out.println("2: " + translation.getText("MAIN_MENU_EXIT"));
-	
+		System.out.println("1: " + translation.getText("MAIN_MENU_START"));
+		System.out.println("2: " + translation.getText("MAIN_MENU_CHANGE_LANGUAGE"));
+		System.out.println("3: " + translation.getText("MAIN_MENU_SEE_SCORE"));
+		System.out.println("4: " + translation.getText("MAIN_MENU_EXIT"));
+		
 	}
 	
 	
@@ -35,9 +38,28 @@ public class Main
 	//
 	public static void main(String[] args)
 	{
-		PrintMainMenu();
+		Scanner scan = new Scanner(System.in);
+		boolean running = true;
 		
 		
+		while(running)
+		{
+			PrintMainMenu();
+			
+			
+			int input = scan.nextInt();
+			switch(input)
+			{
+				case 2:
+					translation.selectLanguage("DE");
+					break;
+				case 4:
+					running = false;
+				break;
+			
+			
+			}
+		}
 	}
 	
 	
